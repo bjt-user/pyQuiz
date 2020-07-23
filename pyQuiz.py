819159076541259b7b1551e39a3d055ecaf942c8
bjt-user <65905537+bjt-user@users.noreply.github.com>
2020-07-23 11:17:27 +0200

    added mistakes counter

--- pyQuiz.py
+++ pyQuiz.py
@@ -33,12 +33,14 @@ answeredQuestions = []
 
 points = 0
 
+mistakes = 0
+
 #booleans seem to be case sensitive and None for defining a bool with no value
 
 #path for the open-function is entered in unix style even on windows systems
 
 try:
-    daten = open("C:/Users/whatever/Documents/unimportant_documents/umsatzsteuerrecht.txt", "r")
+    daten = open("C:/Users/whatever/Documents/unimportant_documents/m_und_a.txt", "r")
     text = daten.read()    
 except:
     print("cannot open file")
@@ -106,9 +108,10 @@ while True:
             points += 1
         else:
             print("the answer in line " + str(n) + " was: " + answerStringList[n])
+            mistakes += 1
 
 
-    print("you have " + str(points) + " points.")
+    print("You have " + str(points) + " points. You made " + str(mistakes) + " mistakes.")
     print()
 
     userInputList.clear()
@@ -127,10 +130,12 @@ input()
 #but i had to proove that i could do it this way
 
 #issue1: Antworten, die sich über mehrere Zeilen erstrecken
-#eine Loesung könnte sein, die userInputList und die answerStringList jeweils in einen String umzuwandeln und dann zu vergleichen ob sie übereinstimmen
+#eine Lösung könnte sein, die userInputList und die answerStringList jeweils in einen String umzuwandeln und dann zu vergleichen ob sie übereinstimmen
 #so wäre es dann egal wann man in einer Antwort den Zeilenumbruch macht
 #wobei das wiederum bei mehrteiligen Antworten, die klar voneinander abzugrenzen sind, nicht so gut wäre
 
 #oder ich mache einfach in der quizdatei einen Zeilenumbruch und replace einfach "\n" mit "" und schließe den answer-tag erst nach mehreren Zeilen,
 #wenn die Antwort wirklich zuende ist
 #nur wie mache ich das dann beim Userinput?
+
+#TODO: print number of current question
